34ad22272a32025741e5eddbf7fb57000acbdd18
AustinStrahm <>
2021-10-05 16:02:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from austin import austinFunction
+
+def answer(txt):
+  answer = austinFunction(txt)
+
+  if answer:
+    return answer
+
+  return "CoreBot cannot respond"
+
+while(True):
+  userInput = input("Input: ")
+  print(answer(userInput))
